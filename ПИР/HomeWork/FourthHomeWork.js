d17f7a838124510b3052d9348ef7d226955a449d
Alexey Vasilev <vasilev.al@elt.cor>
2024-08-27 20:34:11 +0700

    feat: сделан рефактор кода

--- ПИР/HomeWork/FourthHomeWork.js
+++ ПИР/HomeWork/FourthHomeWork.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 
-const SimpleComponent = ({ number, componentRerenderedTimes }) => {
+const SimpleComponent = React.memo(({ number, componentRerenderedTimes }) => {
   componentRerenderedTimes.current++;
 
-  const onPress = () => alert(number);
+  const onPress = useCallback(() => alert(number), [number]);
 
-  return <div onClick={() => onPress()}>Number: {number}</div>;
-};
+  return <div onClick={onPress}>Number: {number}</div>;
+});
 
 export default function App() {
-  const componentRerenderedTimes = React.useRef(0);
-  const [data, setData] = React.useState(
+  const componentRerenderedTimes = useRef(0);
+  const [data, setData] = useState(
     new Array(1000)
       .fill({ number: 0 })
       .map((item, index) => ({ number: item.number, id: String(index + 1) }))
@@ -24,10 +24,10 @@ export default function App() {
   return (
     <div>
       <div>Was rendered: {componentRerenderedTimes.current}</div>
-      <button onClick={() => random()}>random</button>
+      <button onClick={random}>random</button>
       <button
         onClick={() =>
-          setData(data => [{ number: 0, id: Math.random() }, ...data])
+          setData((data) => [{ number: 0, id: Math.random() }, ...data])
         }
       >
         add to top
